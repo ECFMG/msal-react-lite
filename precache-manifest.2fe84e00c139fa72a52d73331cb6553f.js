7e09dc6611009189b9c574ad4519ec81fb30766b
Patrick Gidich <patrick@Patricks-MacBook-Pro.local>
2020-11-12 12:19:05 -0500

    Updates

---
+++ precache-manifest.2fe84e00c139fa72a52d73331cb6553f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b925b754356fd5f800556ab2d94fe5e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1317adbfc86d18a9ec86",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "7ac3fbde8d17f2d84eeb",
+    "url": "./static/js/2.998c8d5d.chunk.js"
+  },
+  {
+    "revision": "f807f051ce188857c0ce39f937998ad8",
+    "url": "./static/js/2.998c8d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1317adbfc86d18a9ec86",
+    "url": "./static/js/main.f98265f5.chunk.js"
+  },
+  {
+    "revision": "100dc40909959562d774",
+    "url": "./static/js/runtime-main.fe4d7596.js"
+  }
+]);
